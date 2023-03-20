385d0d0de736fb30143944c048cc12e1ad604276
STNeto1 <gfreitasneto18@gmail.com>
2023-03-19 22:09:08 -0300

    drizzle error

--- src/db/db.ts
+++ src/db/db.ts
@@ -1,19 +1,20 @@
 import { connect } from "@planetscale/database";
 import { drizzle } from "drizzle-orm/planetscale-serverless";
+import { env } from "~/env.mjs";
 
 // WORKS
-const connection = connect({
-  host: process.env.DATABASE_HOST,
-  username: process.env.DATABASE_USERNAME,
-  password: process.env.DATABASE_PASSWORD,
-});
+// const connection = connect({
+//   host: process.env.DATABASE_HOST,
+//   username: process.env.DATABASE_USERNAME,
+//   password: process.env.DATABASE_PASSWORD,
+// });
 
 // DOES NOT WOTK
-// const connection = connect({
-//   host: env.DATABASE_HOST,
-//   username: env.DATABASE_USERNAME,
-//   password: env.DATABASE_PASSWORD,
-// });
+const connection = connect({
+  host: env.DATABASE_HOST,
+  username: env.DATABASE_USERNAME,
+  password: env.DATABASE_PASSWORD,
+});
 
 export const db = drizzle(connection, {
   logger: true,
